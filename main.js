e03368e3f2772816aff3f58b137e583f8be28265
Nelson <nlparra.dev@gmail.com>
2020-03-04 01:52:49 +0000

    finish javaScript,styling in process

---
+++ main.js
@@ -0,0 +1,37 @@
+ 
+ window.onload = function () {
+ 
+ let pronoun = ['the','our','you','for','they']; 
+ let adj = ['great', 'big','tiny','humongous','little' ]; 
+ let noun = ['jogger','racoon','whale','tiger','dog']; 
+
+
+
+let emptyArray = [];
+for (let i in pronoun)
+    for (let j in adj)
+        for (let k in noun)
+
+emptyArray.push("http://" + pronoun[i] + adj[j] + noun[k] + ".com" + '<br>')
+
+
+console.log(emptyArray);
+document.querySelector("#root").innerHTML = emptyArray
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
